400be995808791fb2d50565e3646d207926b8fdc
sadeemAbd <132435025+sadeemAbd@users.noreply.github.com>
2023-05-04 10:46:59 +0300

    Update Trasport.java

--- Trasport.java
+++ Trasport.java
@@ -4,11 +4,27 @@
  * and open the template in the editor.
  */
 package freelancinggoodstransportation;
-
-/**
- *
- * @author donaa
- */
 public class Trasport {
-    
+    String transport_Ntime;
+    String transport_destenation;
+    String transport_arrival;
+
+
+
+ 
+  public String getNtime() {
+    return transport_Ntime;
+  }
+ public String gettidestnation() {
+    return transport_destenation;
+  }
+
+ public String getarrival() {
+    return transport_arrival;
+  }
+ public String toString() {
+   return "the time:" + this.transport_Ntime + ",, "
+                + "the destenation:" + this.transport_destenation + ",, " + "arrival:"
+                + this.transport_arrival + ",, " ;
+}
 }
